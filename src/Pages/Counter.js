a09c04b77cabcb04949ddcf2acc49e590a3d496b
Chidinma-Edna <108988692+Chidinma-Edna@users.noreply.github.com>
2023-01-07 02:44:54 -0800

    1 file modified

--- src/Pages/Counter.js
+++ src/Pages/Counter.js
@@ -1,32 +1,4 @@
 import React from "react";
-//import "./style.css";
-//import  {useState} from 'react';
-
-//function Counter() {
-//let [countNum, setCount] = useState(0);
-//let increaseValaue = () => {
-//setCount((countNum += 1)); //increases the count by 1
-//};
-//let decreaseValue = () => {
-
-//setCount((countNum -= 1)); 
-
-//};// decreases the count by 1
-//let resetValue = () => {
-//setCount(0); //resets the count to 0
-//};
-//return (
-//<div>
-//<h1> {countNum} </h1>
-//<button onClick={increaseValaue}>+</button> 
-//<button onClick={decreaseValue}>-</button>
-
-//<button onClick={resetValue}>Reset</button>
-//</div>
-//)
-//}
-//export default Counter;
-
 import "./style.css";
 import { useState } from "react";
 
